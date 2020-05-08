30d4a645433fd51d10bccff7e7f3791570d82b63
29244 <29244@ma-web.nl>
2020-05-08 15:13:23 +0200

    07

--- html/02myPolygon/script.js
+++ html/02myPolygon/script.js
@@ -7,48 +7,14 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-
-
-let colors = ["red", "pink", "blue", "green", "purple", "yellow", "orange"];
-
-function getRandomNumber(max){
-  return Math.floor(Math.random()*max);
-}
-
-function spawnCircle(){
-  let myCircle = new Point(getRandomNumber(width), getRandomNumber(height), getRandomNumber(50), colors[getRandomNumber(colors.length)]);
-  myCircle.draw();
-}
-
-function repeat(){
-  spawnCircle();
-}
-
-
 let points = [];
 
 for(let i =0; i<3; i++){
   let x = getRandomInt(0,width);
   let y = getRandomInt(0,height);
-  let myPoint = new Point(x,y,5,"black",i);
+  let myPoint = new Point(x,y,5,"red",i);
   points.push(myPoint);
 }
-
-let myPolygon = new Polygon(point);
-myPolygon.draw(context);
-
-class Polygon{
-  constructor(points){
-    this.points = points;
-    Polygon.add(point)
-  }
-
-  draw(context){
-  }
-
-}
-
-setInterval(repeat,10);
 
 let myPolygon = new Polygon(points);
 myPolygon.draw(context);
