c7f1f915af545546fdf60f74707b7396677fed77
29244 <29244@ma-web.nl>
2020-09-09 10:57:50 +0200

    11.2

--- html/11cogsAndGears/script.js
+++ html/11cogsAndGears/script.js
@@ -7,10 +7,12 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let img,angle;
+let img,img2,angle;
 
 img = new Image();
+img2 = new Image();
 img.src = "images/cog.png";
+img2.src = "images/cog2.png";
 
 
 angle = 0.1;
@@ -24,56 +26,56 @@ function animate(){
   context.save();
   context.translate(40,300);
   context.rotate(-angle+0.3);
-  context.drawImage(img,-150,-150,300,300);
+  context.drawImage(img2,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(320,300);
   context.rotate(angle);
   context.drawImage(img,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(600,300);
   context.rotate(-angle+0.3);
-  context.drawImage(img,-150,-150,300,300);
+  context.drawImage(img2,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(880,300);
   context.rotate(angle);
   context.drawImage(img,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(1160,300);
   context.rotate(-angle+0.3);
-  context.drawImage(img,-150,-150,300,300);
+  context.drawImage(img2,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(1440,300);
   context.rotate(angle);
   context.drawImage(img,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(1720,300);
   context.rotate(-angle+0.3);
-  context.drawImage(img,-150,-150,300,300);
+  context.drawImage(img2,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 
   context.save();
   context.translate(2000,300);
   context.rotate(angle+0.3);
   context.drawImage(img,-150,-150,300,300);
   context.restore();
-  angle +=  0.01;
+  angle +=  0.005;
 }
